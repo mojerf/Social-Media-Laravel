9ffdd24cc4f9081cdbc044f9d0530a01801fe927
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-07-15 18:26:41 +0330

    factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Like;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $posts = Post::factory(100)->recycle($users)->create();
+
+        Comment::factory(200)->recycle($users)->recycle($posts)->create();
+        Like::factory(200)->recycle($users)->recycle($posts)->create();
+
+        User::factory()
+        ->has(Post::factory(10))
+        ->has(Comment::factory(50)->recycle($posts))
+        ->has(Like::factory(50)->recycle($posts))
+        ->create([
+            'username'=>'mojerf',
         ]);
     }
 }
